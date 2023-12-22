9b429fbf72ab8b7dc992016c2ed893ed3e63f86d
FintanSammon <72512712+FintanSammon@users.noreply.github.com>
2023-12-22 12:30:10 +0000

    Update dashboard.js

--- src/components/dashboard.js
+++ src/components/dashboard.js
@@ -1,37 +1,44 @@
+// Import for libraries and components
 import React, { useState, useEffect } from 'react';
-import axios from 'axios';
-import { Doughnut } from 'react-chartjs-2';
-import 'chart.js/auto';
-import './dashboard.css';
-import { getAuth } from 'firebase/auth';
+import axios from 'axios'; // For making HTTP requests
+import { Doughnut } from 'react-chartjs-2'; // Doughnut chart component
+import 'chart.js/auto'; // Auto-import for Chart.js
+import './dashboard.css'; // Importing CSS for styling
+import { getAuth } from 'firebase/auth'; // Firebase authentication
 
+// Dashboard functional component
 function Dashboard() {
+  // State for storing tasks and current user
   const [tasks, setTasks] = useState([]);
   const auth = getAuth();
   const user = auth.currentUser; 
 
+  // Effect to set the initial theme based on local storage
   useEffect(() => {
-    const currentTheme = localStorage.getItem('theme') || 'light';
-    document.body.className = currentTheme;
+    const currentTheme = localStorage.getItem('theme') || 'light'; // Get current theme or default to 'light'
+    document.body.className = currentTheme; // Apply theme to body
   }, []);
 
+  // Effect to fetch tasks when the user changes
   useEffect(() => {
     const fetchTasks = async () => {
       if (!user) {
         console.error('No user logged in');
         return;
       }
+
       try {
         const response = await axios.get(`http://localhost:5000/api/tasks?userUID=${user.uid}`);
-        setTasks(response.data);
+        setTasks(response.data); // Set tasks data from response
       } catch (error) {
         console.error('Error fetching tasks:', error);
       }
     };
 
-    fetchTasks();
+    fetchTasks(); // Fetch tasks
   }, [user]); 
 
+  //  Rendering for non-authenticated users
   if (!user) {
     return (
       <div className="dashboard-container">
@@ -41,29 +48,26 @@ function Dashboard() {
     );
   }
 
+  // Calculating task statistics
   const totalTasks = tasks.length;
   const completedTasks = tasks.filter(task => task.status === 'Complete').length;
   const pendingTasks = totalTasks - completedTasks;
 
+  // Data preparation for the Doughnut chart
   const doughnutChartData = {
     labels: ['Completed Tasks', 'Pending Tasks'],
     datasets: [
       {
         label: '# of Tasks',
         data: [completedTasks, pendingTasks],
-        backgroundColor: [
-          'rgba(75, 192, 192, 0.2)',
-          'rgba(255, 99, 132, 0.2)'
-        ],
-        borderColor: [
-          'rgba(75, 192, 192, 1)',
-          'rgba(255, 99, 132, 1)'
-        ],
+        backgroundColor: ['rgba(75, 192, 192, 0.2)', 'rgba(255, 99, 132, 0.2)'],
+        borderColor: ['rgba(75, 192, 192, 1)', 'rgba(255, 99, 132, 1)'],
         borderWidth: 1,
       },
     ],
   };
 
+  // Rendering the dashboard component
   return (
     <div className="dashboard-container">
       <h2 className="dashboard-header">Dashboard</h2>
@@ -82,12 +86,12 @@ function Dashboard() {
         </div>
       </div>
       <div className="flex-container">
-      <div className="chart-container">
-        <Doughnut data={doughnutChartData} />
+        <div className="chart-container">
+          <Doughnut data={doughnutChartData} /> {/* Rendering the Doughnut chart */}
         </div>
       </div>
     </div>
   );
 }
 
-export default Dashboard;
+export default Dashboard; // Exporting the Dashboard component
