865049193b1cbe1b19fd13220e182f063bc99e71
FintanSammon <72512712+FintanSammon@users.noreply.github.com>
2023-12-22 12:20:53 +0000

    Update Settings.js

--- src/components/Settings.js
+++ src/components/Settings.js
@@ -1,29 +1,35 @@
+// Imports
 import React, { useState, useEffect } from 'react';
 import './settings.css'; 
 
 function Settings() {
-  const [theme, setTheme] = useState(localStorage.getItem('theme') || 'light');
-  const [taskSort, setTaskSort] = useState(localStorage.getItem('taskSort') || 'dueDate');
-  const [dateFormat, setDateFormat] = useState(localStorage.getItem('dateFormat') || 'MM/DD/YYYY');
+  // State variables to manage user settings
+  const [theme, setTheme] = useState(localStorage.getItem('theme') || 'light'); // Initialize theme from local storage or default to 'light'
+  const [taskSort, setTaskSort] = useState(localStorage.getItem('taskSort') || 'dueDate'); // Initialize task sorting from local storage or default to 'dueDate'
+  const [dateFormat, setDateFormat] = useState(localStorage.getItem('dateFormat') || 'MM/DD/YYYY'); // Initialize date format from local storage or default to 'MM/DD/YYYY'
 
+  // Using Effect to update the body class based on the selected theme
   useEffect(() => {
-    document.body.className = theme; 
+    document.body.className = theme; // Setting the body class to the selected theme ('light' or 'dark')
   }, [theme]);
 
+  // Handler function to change the theme
   const handleThemeChange = (newTheme) => {
-    setTheme(newTheme);
-    localStorage.setItem('theme', newTheme);
-    document.body.className = newTheme; 
+    setTheme(newTheme); // Updating the theme state variable
+    localStorage.setItem('theme', newTheme); // Saving the theme preference in local storage for persistence
+    document.body.className = newTheme; // Updating the body class for immediate theme change
   };
 
+  // Handler function to change the task sorting option
   const handleTaskSortChange = (sortOption) => {
-    setTaskSort(sortOption);
-    localStorage.setItem('taskSort', sortOption);
+    setTaskSort(sortOption); // Updating the task sorting state variable
+    localStorage.setItem('taskSort', sortOption); // Saving the sorting preference in local storage
   };
 
+  // Handler function to change the date format
   const handleDateFormatChange = (format) => {
-    setDateFormat(format);
-    localStorage.setItem('dateFormat', format);
+    setDateFormat(format); // Update the date format state variable
+    localStorage.setItem('dateFormat', format); // Saving the date format preference in local storage
   };
 
   return (
@@ -56,4 +62,5 @@ function Settings() {
   );
 }
 
+// Settings Page Export
 export default Settings;
